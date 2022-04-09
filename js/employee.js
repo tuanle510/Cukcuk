f22bb2a54676cceccb93f2b16caadef06da44917
Justin Le <lethientuan8668@gmail.com>
2022-04-09 09:51:11 +0700

    check input
    jquery kiểm tra nếu input trống thì báo lỗi

--- js/employee.js
+++ js/employee.js
@@ -1,19 +1,49 @@
-/**
- * Hiển thị form chi tiết
- * CreatedBy: LTTuan (08.04.2022)
- *
- */
-
 $(document).ready(function () {
-  // gắn các sự kiện cho các element
-  $("#btnCloseDialog").click(function () {
-    $("#addStaff").hide();
-  });
-
-  $("#btnAdd").click(btnAddOnClick);
+  new EmployeePage();
 });
 
-function btnAddOnClick() {
-  $("#addStaff").show();
-}
+class EmployeePage {
+  TitlePage = 'Danh sách nhân viên';
+  constructor() {
+    // khởi tạo các sự kiện
+    this.initEvent();
+  }
 
+  initEvent() {
+    $('#btnShowForm').click(this.btnShowForm);
+    $('.btnCloseForm').click(this.btnCloseForm);
+    $('[required]').blur(this.validateRequired.bind(this));
+  }
+
+  loadData() {}
+
+  validateRequired(event) {
+    this.loadData();
+    var input = event.currentTarget;
+    console.log(input);
+    // kiểm tra giá trị của input có hay không?
+    var value = $(input).val();
+    // Nếu không có thì hiện thị trạng thái lỗi
+    if (value == null || value == '') {
+      $(input).addClass('m-input-error');
+      $(input).attr('title', 'Thông tin này không được để trống');
+    } else {
+      $(input).removeClass('m-input-error');
+      $(input).removeAttr('title');
+    }
+  }
+
+  /**
+   * Hiển thị form chi tiết
+   * CreatedBy: LTTuan (08.04.2022)
+   *
+   */
+
+  btnShowForm() {
+    $('#addStaff').show();
+  }
+
+  btnCloseForm() {
+    $('#addStaff').hide();
+  }
+}
